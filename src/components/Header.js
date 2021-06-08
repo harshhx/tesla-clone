504254ec3b71454b3d76c9968d0062466c049aa1
harshhx <hg070401@gmail.com>
2021-06-09 02:06:46 +0530

    working burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from "styled-components"
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+ 
 
-function header() {
+function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false)
     return (
         <Container>
             <a href="#">
@@ -26,17 +29,32 @@ function header() {
             <a href="#">
                 Tesla Accounts
             </a>
-            <MenuIconContainer>
+            <MenuIconContainer onClick={()=> setBurgerStatus(true)}>
                 <MenuIcon></MenuIcon>
             </MenuIconContainer>
         </RightMenu>
+        <BurgerNav status = {burgerStatus}>
+            <CloseContainer onClick={()=> setBurgerStatus(false)}>
+                <Close />
+            </CloseContainer>
+            
+            <li><a href="#">Model S</a></li>
+            <li><a href="#">Model Y</a></li>
+            <li><a href="#">Existing Inventory</a></li>
+            <li><a href="#">Used Inventory</a></li>
+            <li><a href="#">Trade In</a></li>
+            <li><a href="#">CyberTruck</a></li>
+            <li><a href="#">Roadster</a></li>
+            <li><a href="#">Semi</a></li>
+            <li><a href="#">Charging</a></li>  
+        </BurgerNav>
         </Container>
 
         
     )
 }
 
-export default header
+export default Header
 
 const Container = styled.div`
     display:flex;
@@ -48,6 +66,7 @@ const Container = styled.div`
     top:0;
     left: 0;
     right:0;
+    z-index: 10;
     
 `
 const MenuGroup = styled.div`
@@ -57,6 +76,10 @@ const MenuGroup = styled.div`
         font-weight :600;
         padding: 0 10px;
         cursor: pointer;
+    }
+
+    @media(max-width:768px){
+        display: none;
     }
 `
 const RightMenu = styled.div`
@@ -77,4 +100,35 @@ const MenuIconContainer = styled.div`
     align-items: center;
     cursor: pointer;
 
+`
+
+const BurgerNav = styled.div`
+    position: fixed;
+    width: 300px;
+    background: white;
+    top:0;
+    right:0;
+    bottom: 0;
+    padding: 20px;
+    list-style: none;
+    transform: ${props => props.status ? 'translateX(0)': 'translateX(100%)'};
+    transition: transform 0.2s ease-in;
+    li{
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0,0,0,.2);
+    }
+    a{
+        font-weight: 600;
+        text-decoration: none;
+    }
+
+`
+
+const CloseContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`
+
+const Close = styled(CloseIcon)`
+    cursor: pointer;
 `
